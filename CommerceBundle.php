5c96a3aa4df1ce1e133cd6b19a52f872e33a520b
Michael Durand <michael.durand.md@gmail.com>
2019-10-15 01:43:20 +0200

    add container builder

--- CommerceBundle.php
+++ CommerceBundle.php
@@ -2,9 +2,14 @@
 
 namespace Appeweb\CommerceBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CommerceBundle extends Bundle
 {
-    
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
